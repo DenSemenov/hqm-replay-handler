589205048cc527cb8fe0f5ba88755383ebcb20b8
Denis Semenov <denis.semenov@pix.ru>
2024-05-22 21:53:31 +0300

    Moved to console

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,10 +1,32 @@
 ﻿using hqm_ranked_backend.Helpers;
 using hqm_replay_handler;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
 
-var filePath = "D:\\test.hrp";
-var data = File.ReadAllBytes(filePath);
-var result = Replay.ParseReplay(data);
+Start:
+Console.WriteLine("Enter or paste path to hrp file");
+var path = Console.ReadLine();
+if (File.Exists(path))
+{
+    Console.WriteLine("Processing file");
+    var data = File.ReadAllBytes(path);
+    var result = Replay.ParseReplay(data);
 
-var processedData = ReplayDataHelper.GetReplayCalcData(result);
+    Console.WriteLine("Calculating data");
 
-Console.WriteLine();
+    var processedData = ReplayDataHelper.GetReplayCalcData(result);
+
+    var jsonResult = JsonConvert.SerializeObject(processedData);
+    var outputPath = path.Replace(".hrp", ".json");
+    File.WriteAllText(outputPath, jsonResult);
+
+    Console.WriteLine("File saved to {0}", outputPath);
+
+    goto Start;
+}
+else
+{
+    Console.WriteLine("File not exists");
+
+    goto Start;
+}
